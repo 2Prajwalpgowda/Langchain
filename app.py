f80519ce5e654a240c4c95ef4f8cc61b3e235907
2Prajwalpgowda <161187114+2Prajwalpgowda@users.noreply.github.com>
2024-02-26 12:56:55 +0530

    HG

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+
+from langchain_openai import OpenAI
+
+#Function to return the response
+def load_answer(question):
+    
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct",temperature=0)
+
+    answer=llm.invoke(question)
+    return answer
+
+#App UI starts here
+st.set_page_config(page_title="Langchain Q&A", page_icon=":robot:")
+st.header("Langchain Q&A")
+
+#Gets the user input
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+
+user_input=get_text()
+response = load_answer(user_input)
+
+submit = st.button('Generate')  
+
+#If generate button is clicked
+if submit:
+
+    st.subheader("Answer:")
+
+    st.write(response)
+
+
